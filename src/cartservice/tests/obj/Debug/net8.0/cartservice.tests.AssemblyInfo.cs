c601bc55a4d7aa421f7b17183c8fd51138b09e7b
Ahmed Salem <ahmedsalem@BGIT00128.local>
2024-08-25 12:38:49 +0400

    Add terraform-admin-key.json to .gitignore

--- src/cartservice/tests/obj/Debug/net8.0/cartservice.tests.AssemblyInfo.cs
+++ src/cartservice/tests/obj/Debug/net8.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3561172ec47d4272a6fd43f036c3b15734577d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
